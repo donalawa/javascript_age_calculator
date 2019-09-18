207883c9d3d41c9e589ff1c25cb2a8d7a1437a6f
donalawa <awadonalcien12@gmail.com>
2019-09-18 12:41:46 +0100

    This is my age calculator

---
+++ age.js
@@ -0,0 +1,13 @@
+function agecalc(){
+	var y_birth = document.getElementById("age").value;
+	y_birth = Number(y_birth);
+
+	if(typeof(y_birth) === "number"){
+		var c_year = 2019;
+		var y_age = c_year - y_birth;
+		//alert(y_age)
+		document.getElementById("display").innerHTML = "You are " + y_age + " years old";
+	}else{
+		alert("Its not a number");
+	}
+}
