01adf28a9d965d99a959359fe8682b5ca0e3eb7f
donalawa <54797997+donalawa@users.noreply.github.com>
2019-12-30 14:44:08 +0100

    Update age.js

--- age.js
+++ age.js
@@ -1,7 +1,7 @@
 function agecalc(){
 	var y_birth = document.getElementById("age").value;
 	y_birth = Number(y_birth);
-   
+       
 	if(typeof(y_birth) === "number"){
 		var c_year = 2019;
 		var y_age = c_year - y_birth;
